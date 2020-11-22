597fb87f06a7866b3af71fe3887e3cc243f03fcd
DanielVanDijkJacobsen <danilar@ruc.dk>
2020-11-22 22:06:47 +0100

    A little bit of html and added data handler for AJAX func

---
+++ WebService/WWWroot/js/dataHandler.js
@@ -0,0 +1,11 @@
+﻿
+//Function for getting list of popular titles
+let getPopular = function (callback) {
+    fetch("api/titles/popular")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            callback(data);
+        });
+};
