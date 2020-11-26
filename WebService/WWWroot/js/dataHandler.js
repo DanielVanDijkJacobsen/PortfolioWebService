37a58dbef2c271e2e719b4c2152f2e3d2ec8dcfb
DanielVanDijkJacobsen <danilar@ruc.dk>
2020-11-26 14:19:27 +0100

    Update dataHandler.js

--- WebService/WWWroot/js/dataHandler.js
+++ WebService/WWWroot/js/dataHandler.js
@@ -4,6 +4,14 @@
 
 define([], () => {
 
+    let getBookmarks = (userId, callback) => {
+        fetch("api/bookmarks" + userId)
+            .then(response => response.json())
+            .then(callback);
+    }
+
+    //How to call other functions than get api's
+
     let getPopular = (callback) => {
         fetch("api/titles/popular")
             .then(response => response.json())
@@ -23,6 +31,7 @@ define([], () => {
     }
 
     return {
+        getBookmarks,
         getPopular,
         getTitles,
         getTitle
