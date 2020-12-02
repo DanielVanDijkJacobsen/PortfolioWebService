961870280129e38fd2062e11e4064172fc761d1b
DanielVanDijkJacobsen <danilar@ruc.dk>
2020-12-02 21:49:55 +0100

    Expanded upon dataService

--- WebService/WWWroot/js/dataHandler.js
+++ WebService/WWWroot/js/dataHandler.js
@@ -1,8 +1,7 @@
-﻿
-//Function for getting list of popular titles
+﻿define([], () => {
 
-
-define([], () => {
+    //Unsure if entity framework (bookmarks, comment and like) has all of its functionality.
+    //Unsure if casts query is still undefined.
 
     let getBookmarks = (userId, callback) => {
         fetch("api/bookmarks" + userId)
@@ -10,7 +9,38 @@ define([], () => {
             .then(callback);
     }
 
-    //How to call other functions than get api's
+    let getCastInfo = (id, callback) => {
+        fetch("api/castinfo" + id)
+            .then(response => response.json())
+            .then(callback);
+    }
+
+    let getCastInfos = (callback) => {
+        fetch("api/castinfo")
+            .then(response => response.json)
+            .then(callback);
+    }
+
+    let getCasts = (id, callback) => {
+        fetch("api/casts" + id)
+            .then(response => response.json())
+            .then(callback);
+    }
+
+    //Comments
+    //FlaggedComments
+
+    let getFormat = (id, callback) => {
+        fetch("api/formats" + id)
+            .then(response => response.json())
+            .then(callback);
+    }
+
+    let getFormats = (callback) => {
+        fetch("api/formats")
+            .then(response => response.json())
+            .then(callback);
+    }
 
     let getPopular = (callback) => {
         fetch("api/titles/popular")
@@ -18,22 +48,28 @@ define([], () => {
             .then(callback);
     };
 
-    let getTitles = (callback) => {
-        fetch("api/titles")
-            .then(response => response.json())
-            .then(callback);
-    }
-
     let getTitle = (id, callback) => {
         fetch("api/categories" + id)
             .then(response => response.json)
             .then(callback);
     }
 
+    let getTitles = (callback) => {
+        fetch("api/titles")
+            .then(response => response.json())
+            .then(callback);
+    }
+
+
     return {
         getBookmarks,
+        getCastInfo,
+        getCastInfos,
+        getCasts,
+        getFormat,
+        getFormats,
         getPopular,
-        getTitles,
-        getTitle
+        getTitle,
+        getTitles
     }
 });
