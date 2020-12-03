7f1992c94bee4dc836641976017d3fbefa999921
DanielVanDijkJacobsen <danilar@ruc.dk>
2020-12-03 21:29:35 +0100

    Update dataHandler.js
    Sorting, commenting and cleaning

--- WebService/WWWroot/js/dataHandler.js
+++ WebService/WWWroot/js/dataHandler.js
@@ -1,14 +1,11 @@
 ﻿define([], () => {
 
-    //Unsure if entity framework (bookmarks, comment and like) has all of its functionality.
-    //Unsure if casts query is still undefined.
-
+    //Bookmarks
     let getBookmarks = (userId, callback) => {
         fetch("api/bookmarks" + userId)
             .then(response => response.json())
             .then(callback);
     }
-
     let createBookmarkTitle = (userId, callback, titleId) => {
         let headers = new Headers();
         headers.append("Content-Type", "application/json");
@@ -16,21 +13,19 @@
             .then(response => response.json)
             .then(data => callback(data));
     }
-
     let deleteBookmark = url => fetch(url, { method: "DELETE" });
 
+    //Cast information
     let getCastInfo = (id, callback) => {
         fetch("api/castinfo" + id)
             .then(response => response.json())
             .then(callback);
     }
-
     let getCastInfos = (callback) => {
         fetch("api/castinfo")
             .then(response => response.json)
             .then(callback);
     }
-
     let getCasts = (id, callback) => {
         fetch("api/casts" + id)
             .then(response => response.json())
@@ -40,53 +35,44 @@
     //Comments
     //FlaggedComments
 
+    //Title formats and genres
     let getFormat = (id, callback) => {
         fetch("api/formats" + id)
             .then(response => response.json())
             .then(callback);
     }
-
     let getFormats = (callback) => {
         fetch("api/formats")
             .then(response => response.json())
             .then(callback);
     }
-
     let getGenre = (id, callback) => {
         fetch("api/genre" + id)
             .then(response => response.json())
             .then(callback);
     }
-
     let getGenres = (callback) => {
         fetch("api/genre")
             .then(response => response.json())
             .then(callback);
     }
 
+    //Cast known for, ratings and professions
     let getKnownFor = (id, callback) => {
         fetch("api/knownfor" + id)
             .then(response => response.json)
             .then(callback);
     }
-
     let getNameRatings = (id, callback) => {
         fetch("api/nameratings" + id)
             .then(response => response.json)
             .then(callback);
     }
-
     let getProfessions = (id, callback) => {
         fetch("api/professions" + id)
             .then(response => response.json)
             .then(callback);
     }
-
-    let getPopular = (callback) => {
-        fetch("api/titles/popular")
-            .then(response => response.json())
-            .then(callback);
-    };
 
     //Ratings
 
@@ -94,29 +80,32 @@
 
     //SearchHistory
 
+    //Title information
     let getTitleAlias = (id, callback) => {
         fetch("api/titlealias" + id)
             .then(response => response.json)
             .then(callback);
     }
-
     let getTitleInfo = (id, callback) => {
         fetch("api/titleinfo" + id)
             .then(response => response.json)
             .then(callback);
     }
-
     let getTitle = (id, callback) => {
         fetch("api/categories" + id)
             .then(response => response.json)
             .then(callback);
     }
-
     let getTitles = (callback) => {
         fetch("api/titles")
             .then(response => response.json())
             .then(callback);
     }
+    let getPopular = (callback) => {
+        fetch("api/titles/popular")
+            .then(response => response.json())
+            .then(callback);
+    };
 
     //Users
 
